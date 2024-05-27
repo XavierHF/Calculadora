4f7ba6720c16856d617d28b8fa26c441cebb6e23
XavierHF <reivax_slayer123@hotmail.com>
2024-05-27 16:35:38 -0600

    Quite logo

--- src/App.js
+++ src/App.js
@@ -23,13 +23,7 @@ function App() {
   };
 
   return (
-    <div className='App'>
-      <div className='freecodecamp-logo-contenedor'>
-        <img 
-          src={freeCodeCampLogo}
-          className='freecodecamp-logo'
-          alt='Logo de freeCodeCamp' />
-      </div>
+    <div className='App'>|
       <div className='contenedor-calculadora'>
         <Pantalla input={input}/>
         <div className='fila'>
